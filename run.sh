ad4e59bc1104735443eb456aa69c4bfa02ed7399
Etienne Vivet <etienne.vivet@edu.devinci.fr>
2021-03-12 11:21:16 +0000

    Exercice 4

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
-python3 -m venv .myenv
+FILE=/home/ubuntu/Project/projectGitPython_VIVET_SARROT/.myenv
+if [ ! -e "$FILE" ]; then
+	python3 -m venv .myenv
+	echo "environnement crée"
+else
+	echo "environnement pré-existant"
+fi
 source .myenv/bin/activate
 pip install -r requirements.txt
 python main.py
